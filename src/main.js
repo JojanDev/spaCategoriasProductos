35d9250b2806fcec1dc7680fea8ccce885e15b24
Johan Delgado <johan24.dev@gmail.com>
2025-06-13 12:02:16 -0500

    Enrutador

--- src/main.js
+++ src/main.js
@@ -1,2 +1,54 @@
+//Importaciones
 import './style.css';
 import 'remixicon/fonts/remixicon.css';
+import * as categoryController from './views/category/categoryController.js';
+import * as productController from './views/product/productController.js';
+import * as homeController from './views/home/homeController.js';
+
+//Variables
+const main = document.querySelector('.main');
+
+const rutas = [
+  {
+    name: "category",
+    path: "/src/views/category/index.html",
+    src: categoryController
+  },
+  {
+    name: "product",
+    path: "/src/views/product/index.html",
+    src: productController
+  },
+  {
+    name: "home",
+    path: "/src/views/home/index.html",
+    src: homeController
+  }
+];
+
+//Funciones
+const get = async(folder) => {
+  const response = await fetch(`src/views/${folder}/index.html`);
+  const data = await response.text();
+  return data;
+}
+
+const enrutador = async (locationHash) => {
+  const hash = locationHash.slice(1);
+
+  const vista = rutas.find(({ name }) => name == hash);
+  console.log(vista);
+
+  main.innerHTML = await get(vista.name);
+  // window.location.hash = vista.name + vista.path;
+}
+
+//Eventos
+document.addEventListener('DOMContentLoaded', () => {
+  enrutador(location.hash);
+
+  //Hashchange
+  window.addEventListener('hashchange', async(e) => {
+    enrutador(location.hash);
+  })
+})
